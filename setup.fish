20836c074b150a2f89db708c06da0b23bbf9c56d
Seungjin Yang <slowmoyang@gmail.com>
2026-03-26 15:21:05 +0900

    build: remove micromamba environment in favor of uv
    Deprecate micromamba-based setup:
    - Remove environment.yaml (conda/micromamba dependency spec)
    - Remove setup.sh and setup.fish (micromamba activation scripts)
    
    The project now uses uv for environment and dependency management, as reflected in pyproject.toml and uv.lock.

--- setup.fish
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env fish
-set -xg PROJECT_ROOT (dirname (readlink -m (status --current-filename)))
-set -xga PYTHONPATH {$PROJECT_ROOT}/src
-eval "$(micromamba shell hook --shell fish)"
-micromamba activate muonly-py312
